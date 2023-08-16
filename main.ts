3bfe854f1d5b36892d82d15e8828c406b132da3a
holterdipolter <simonwedel@gmx.de>
2023-08-16 14:37:49 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-basic.showString(serial.readString())
+let string = ""
+radio.setGroup(1)
 basic.forever(function () {
-	
+    string = serial.readString()
+    serial.writeLine("Rueckgabe: " + string)
+    radio.sendString(string)
+    basic.showString(string)
 })
